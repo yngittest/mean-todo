90b4aa7cd82a36ebc7b65b566688a6cdff6c0425
yngittest <vagrant@localhost.localdomain>
2017-06-04 04:46:50 +0900

    add files for reminder

---
+++ server/components/ifttt.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import request from 'request';
+
+export default function(eventId, key, value) {
+  const url = `https://maker.ifttt.com/trigger/${eventId}/with/key/${key}`;
+
+  const headers = {
+    'Content-Type': 'application/json'
+  };
+
+  var options = {
+    url,
+    method: 'POST',
+    headers,
+    json: true,
+    form: {
+      value1: value[0],
+      value2: value[1],
+      value3: value[2]
+    }
+  };
+
+  request(options, function(error, response, body) {
+    if(!error && response.statusCode == 200) {
+      console.log(body);
+    } else {
+      console.log(`error: ${response.statusCode}`);
+    }
+  });
+}
---
+++ server/components/reminder.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const moment = require('moment');
+
+import Todo from '../api/todo/todo.model';
+import ifttt from './ifttt';
+
+export default function() {
+  const searchDate = new Date();
+  Todo
+    .find({ done: false })
+    .populate('user', 'iftttKey')
+    .then(function(todos) {
+      let targetTodos = todos.filter(function(todo) {
+        return requiresNotification(todo, searchDate);
+      });
+      if(targetTodos.length > 0) {
+        sendNotifications(targetTodos);
+      }
+    });
+}
+
+function requiresNotification(todo, date) {
+  let diff = moment(todo.due).diff(moment(date), 'minutes', true);
+  return diff >= 0 && diff < 1;
+}
+
+function sendNotifications(todos) {
+  todos.forEach(function(todo) {
+    const eventId = 'todoReminder';
+    let key = todo.user.iftttKey;
+    let value = [
+      todo.name,
+      moment(todo.due).format("YYYY/MM/DD HH:mm"),
+      ''
+    ];
+    ifttt(eventId, key, value);
+  });
+}
